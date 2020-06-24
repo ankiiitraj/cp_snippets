bdd2ef9a7f877a7ed2dc0223ca104505d773a810
SHAURY SRIVASTAV <66357200+shaurysrivastav27@users.noreply.github.com>
2020-06-24 19:49:16 +0530

    Update combimatrix.py
    I have coverted the above function into a snippet.

--- combimatrix.py
+++ combimatrix.py
@@ -1,6 +1,8 @@
 from itertools import product
-#a=2d array
-a=[[1,2,3],[1,2,3]]
-#all possible combinations are 
-b=list(product(*a))
-print(b)
+def function(list):
+	#a=2d array
+	list.append(list)
+	#all possible combinations are 
+	b=list(product(*list))
+	return b
+	
